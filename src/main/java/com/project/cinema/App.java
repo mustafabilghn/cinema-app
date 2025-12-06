dd7d6493650913381f39f3781d85aa7c97cbd8ef
mustafabilghn <mustafabilgehanates@hotmail.com>
2025-12-06 23:14:39 +0300

    Add ThreeDMovie class with basic pricing

--- src/main/java/com/project/cinema/App.java
+++ src/main/java/com/project/cinema/App.java
@@ -2,13 +2,16 @@ package com.project.cinema;
 
 import com.project.cinema.models.TwoDMovie;
 import com.project.cinema.models.Movie;
+import com.project.cinema.models.ThreeDMovie;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Movie movie = new TwoDMovie("Interstellar", 250, "fiction", 8.5);
-        System.out.println("Movie: " + movie.getTitle());
-        System.out.println("Price: " + movie.getPrice());
+        Movie m1 = new TwoDMovie("Interstellar", 250, "fiction", 8.5);
+        Movie m2 = new ThreeDMovie("Avatar", 162, "Fantasy", 7.8);
+
+        System.out.println("Movie: " + m1.getTitle() + ", Price: " + m1.getPrice());
+        System.out.println("Movie: " + m2.getTitle() + ", Price: " + m2.getPrice());
     }
 }
