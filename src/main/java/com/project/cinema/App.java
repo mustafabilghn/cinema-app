006fce57c0f058bbed67da1f3d8fd572c43167d4
mustafabilghn <mustafabilgehanates@hotmail.com>
2025-12-21 23:55:31 +0300

    Add CinemaService class

--- src/main/java/com/project/cinema/App.java
+++ src/main/java/com/project/cinema/App.java
@@ -1,29 +1,26 @@
 package com.project.cinema;
 
 import com.project.cinema.models.TwoDMovie;
+
+import java.util.Arrays;
+
+import com.project.cinema.models.CinemaService;
+import com.project.cinema.models.Customer;
 import com.project.cinema.models.Movie;
 import com.project.cinema.models.ShowTime;
-import com.project.cinema.models.ThreeDMovie;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Movie m1 = new TwoDMovie("Interstellar", 250, "fiction", 8.5);
-        Movie m2 = new ThreeDMovie("Avatar", 162, "Fantasy", 7.8);
+        CinemaService cinemaService = new CinemaService();
+        Customer customer = new Customer("Mustafa");
+        Movie movie = new TwoDMovie("Inception", 148, "Sci-Fi", 8.8);
+        ShowTime showTime = new ShowTime(movie,"18:00", 10);
 
-        System.out.println("Movie: " + m1.getTitle() + ", Price: " + m1.getPrice());
-        System.out.println("Movie: " + m2.getTitle() + ", Price: " + m2.getPrice());
-
-        ShowTime show1 = new ShowTime(m1, "18:00", 10);
-        ShowTime show2 = new ShowTime(m2, "20:00", 10);
-
-        System.out.println("ShowTime for " + show1.getMovie().getTitle() + " at " + show1.getTime());
-        System.out.println("Available Seats: " + show1.getAvailableSeats().size());
-        show1.getSeats().get(0).reserve();
-        System.out.println("Available Seats:" + show1.getAvailableSeats().size());
-
-        System.out.println("ShowTime for " + show2.getMovie().getTitle() + " at " + show2.getTime());
-        System.out.println("Available Seats: " + show2.getAvailableSeats().size());
+        cinemaService.printAvailableSeats(showTime);
+        cinemaService.reserveSeats(customer, showTime, Arrays.asList(3, 5, 7));   
+        cinemaService.printAvailableSeats(showTime);
+        cinemaService.printBookingHistory();
     }
 }
